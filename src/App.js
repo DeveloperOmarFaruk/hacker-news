b4c44045b2166cd93cd414c9993c25148ec061ba
Omar Faruk <omarfaruk18115@gmail.com>
2021-05-05 12:35:16 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom'
+import Hacker from './Components/Hacker/Hacker'
+
+import { GlobalStyle } from './GlobalStyle'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( 
+        < Router >
+        <GlobalStyle />
+        <Hacker />
+        </Router>
+    );
 }
-
 export default App;
+
+
+
+ //Develop by Md. Omar Faruk Tutul
